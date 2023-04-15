01bfec36a3218f66ce33503750338a25cd4f5858
jared <jtlss14@gmail.com>
2023-04-15 11:36:36 +0100

    add update_default_version to launch template

--- modules/launch-template/main.tf
+++ modules/launch-template/main.tf
@@ -1,7 +1,8 @@
 resource "aws_launch_template" "test_launch_template" {
-  name          = var.launch_template_name
-  instance_type = var.instance_type
-  image_id      = var.ami_id
+  name                   = var.launch_template_name
+  instance_type          = var.instance_type
+  image_id               = var.ami_id
+  update_default_version = true
 
   user_data = filebase64("${path.module}/user-data/bootstrap.sh")
 }
